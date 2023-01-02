3a327b2a584a0fae2d51774c1df15caefe582871
Your Name <you@example.com>
2023-01-02 22:31:52 +0530

    Adding new changes

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -8,6 +8,7 @@ const Task = ({task,onDelete,onToggle}) => {
           {task.text}  
           <FaTimes style={{color:'red', cursor:'pointer'}} onClick={()=>onDelete(task.id)} /> 
         </h3>
+        <p>{task.day}</p>
     </div>
   )
 }
