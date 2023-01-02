2a9aca43e56356f08d1e1ee55d91bf22c5ea8be0
Your Name <you@example.com>
2023-01-03 00:58:57 +0530

    Adding toggleReminder for json-server

--- src/App.js
+++ src/App.js
@@ -22,6 +22,12 @@ function App() {
     return data
   }
 
+  // Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    return data
+  }
 
   //Add Task
   const addTask = async (task) => { 
@@ -52,7 +58,23 @@ function App() {
   }
 
   //Toggle Reminder
-  const toggleReminder = (id) => { 
+  // const toggleReminder = async (id) => { 
+  //   const taskToToggle = await fetchTask(id)
+  //   const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder}
+    
+  //   const res = await fetch(`http://localhost:5000/${id}`,{
+  //     method:'PUT',
+  //     headers: {
+  //       'Content-type':'application/json'
+  //     },
+  //     body: JSON.stringify(updTask)
+  //   })
+
+  //   const data = await res.json()
+  //   setTasks(tasks.map( (task) =>  task.id === id ? {...task, reminder: data.reminder} :task  ))
+  // }
+
+  const toggleReminder =  (id) => { 
     setTasks(tasks.map( (task) =>  task.id === id ? {...task, reminder: !task.reminder} :task  ))
   }
 
